a42680106bb8c3077ec7a36c1175015900545ccd
jgeun <jgeun@wavve.com>
2023-11-28 18:00:30 +0900

    chore: 폰트 추가

---
+++ app/src/main/java/com/wedge/drawingtoyou/ui/theme/Font.kt
@@ -0,0 +1,11 @@
+package com.wedge.drawingtoyou.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.wedge.drawingtoyou.R
+
+val devomayo = FontFamily(
+    Font(R.font.dovemayo_gothic, FontWeight.Normal, FontStyle.Normal)
+)
